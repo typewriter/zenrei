a82b5aa2611c831e7f16d4441803eb1384ec5f94
typewriter <takuyaxp@gmail.com>
2019-02-21 00:08:19 +0900

    [WIP] Implement /search API

--- api/main.go
+++ api/main.go
@@ -1,31 +1,48 @@
 package main
 
 import (
-  "net/http"
-  "github.com/labstack/echo"
-  "github.com/globalsign/mgo"
-  "github.com/globalsign/mgo/bson"
+	"strings"
+	"net/http"
+	"github.com/labstack/echo"
+	"github.com/globalsign/mgo"
+	"github.com/globalsign/mgo/bson"
 )
 
 type Name struct {
-  Repository string `bson:"repository"`
-  Filename   string `bson:"filename"`
-  Type       int    `bson:"type"`
-  Name       string `bson:"name"`
+	Repository string `bson:"repository"`
+	Filename   string `bson:"filename"`
+	Type       int    `bson:"type"`
+	Name       string `bson:"name"`
 }
 
-func main() {
-  e := echo.New()
-  e.GET("/", func(c echo.Context) error {
-    session, _ := mgo.Dial("mongo")
-    collection := session.DB("zenrei").C("names")
+var collection *mgo.Collection
 
-    var items []Name
-    //_ = collection.Find(nil).Limit(10000).All(&items)
-    //_ = collection.Find(bson.M{"repository":"ruby/ruby"}).Limit(100).All(&items)
-    _ = collection.Find(bson.M{"name":"hoge"}).Limit(100).All(&items)
-    return c.JSON(http.StatusOK, items)
-  })
-  e.Logger.Fatal(e.Start(":8080"))
+func main() {
+	echo := echo.New()
+
+	session, _ := mgo.Dial("mongo")
+	collection  = session.DB("zenrei").C("names")
+
+	echo.GET("/search", search)
+
+	echo.Logger.Fatal(echo.Start(":8080"))
+}
+
+func search(c echo.Context) error {
+	q := strings.Split(c.QueryParam("q"), ",")
+
+	results := make(map[string][]Name)
+	for _, value := range q {
+		var items []Name
+		_ = collection.Find(
+			bson.M{ "$and": []bson.M {
+				bson.M{"name": value},
+				bson.M{"filename": bson.M{"$not": bson.RegEx{"test/|spec/", ""}}},
+			}}).
+			Limit(1000).All(&items)
+		results[value] = items
+	}
+
+	return c.JSON(http.StatusOK, results)
 }
 
