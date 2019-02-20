9b04f956b285b236b181f52b90f343c4a1c4db5a
typewriter <takuyaxp@gmail.com>
2019-02-21 00:02:24 +0900

    Expand coverage of repository

--- creator/retreiver.rb
+++ creator/retreiver.rb
@@ -8,11 +8,27 @@ WORKING_DIR = "#{BASE_DIR}/data"
 Dir::mkdir(WORKING_DIR) if !Dir.exists?(WORKING_DIR)
 
 def update_repository_list
-  1.upto(1) { |page_index|
+  puts "update repository list..."
+  repositories = nil
+  1.upto(100) { |page_index|
+    print "page #{page_index}: "
+
     #url = "https://api.github.com/search/repositories?q=stars%3A>1&sort=stars&order=desc&page=#{page_index}"
     url = "https://api.github.com/search/repositories?q=language%3ARuby&sort=stars&order=desc&page=#{page_index}"
-    `curl "#{url}" > #{WORKING_DIR}/repositories.json`
+    repos = JSON.parse(`curl "#{url}"`)
+
+    break if !repos || !repos["items"]
+
+    if repositories
+      repositories["items"] += repos["items"]
+    else
+      repositories = repos
+    end
+
+    puts "#{repositories["items"].count}..."
+    sleep 10
   }
+  File.open("#{WORKING_DIR}/repositories.json", "w") { |f| f.print repositories.to_json }
 end
 
 def read_repository_list
@@ -33,7 +49,7 @@ def clone_or_pull_repositories(repository_list)
       `git clone --depth 1 #{clone_url} #{clone_path}`
     end
 
-    sleep SLEEP_INTERVAL
+    sleep SLEEP_INTERVAL * (1+rand*2)
   }
 end
 
