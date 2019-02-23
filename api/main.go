d27ba464f2a99bf9d43bba00d1070e7ead9c8875
typewriter <takuyaxp@gmail.com>
2019-02-23 12:10:15 +0900

    [WIP] Add /suggest API

--- api/main.go
+++ api/main.go
@@ -15,17 +15,40 @@ type Name struct {
 	Name       string `bson:"name"`
 }
 
+type Suggest struct {
+	Name       string `bson:"name"`
+	Type       int    `bson:"type"`
+	Count      int    `bson:"count"`
+}
+
 var collection *mgo.Collection
+var countCollection *mgo.Collection
 
 func main() {
 	echo := echo.New()
 
 	session, _ := mgo.Dial("mongo")
-	collection  = session.DB("zenrei").C("names")
+	collection       = session.DB("zenrei").C("names")
+	countCollection  = session.DB("zenrei").C("counters")
 
 	echo.GET("/search", search)
+	echo.GET("/suggest", suggest)
 
 	echo.Logger.Fatal(echo.Start(":8080"))
+}
+
+func suggest(c echo.Context) error {
+  q := c.QueryParam("q")
+
+	var results []Suggest
+	_ = countCollection.Find(
+		bson.M{ "$and": []bson.M {
+			bson.M{"name": bson.RegEx{"^" + q, "i"}},
+			bson.M{"type": nil},
+		}},
+	).Sort("-count").Limit(100).All(&results)
+
+	return c.JSON(http.StatusOK, results)
 }
 
 func search(c echo.Context) error {
