cf5af46ae3705ef9d283815f3bfa2755676fd132
typewriter <takuyaxp@gmail.com>
2019-02-28 00:57:42 +0900

    Implement synonym view and loading spinner

--- view/src/components/Footer.vue
+++ view/src/components/Footer.vue
@@ -2,9 +2,15 @@
   <b-container classs="footer" fluid>
     <b-row class="bg-light pt-2 pb-2">
       <b-col cols="12">
-        <b-badge variant="primary">Developed by </b-badge> typewriter<br>
-        <b-badge variant="primary">Website</b-badge>&nbsp;<a href="https://www.nyamikan.net">にゃみかん (www.nyamikan.net)</a><br>
-        <b-badge variant="primary">GitHub</b-badge>&nbsp;<a href="https://github.com/typewriter/zenrei">typewriter / zenrei</a>
+        <b-badge variant="white">Developed by </b-badge> typewriter<br>
+        <b-badge variant="white">Website</b-badge>&nbsp;<a href="https://www.nyamikan.net">にゃみかん (www.nyamikan.net)</a><br>
+        <b-badge variant="white">GitHub</b-badge>&nbsp;<a href="https://github.com/typewriter/zenrei">typewriter / zenrei</a>
+      </b-col>
+    </b-row>
+    <b-row class="bg-light pt-2 pb-2">
+      <b-col cols="12">
+        <b-badge variant="white">ライセンス表示</b-badge>&nbsp;
+        <a href="http://compling.hss.ntu.edu.sg/wnja/index.ja.html">Japanese Wordnet (1.1) © 2009-2011 NICT, 2012-2015 Francis Bond and 2016-2017 Francis Bond, Takayuki Kuribayashi</a>
       </b-col>
     </b-row>
   </b-container>
--- view/src/components/SearchBar.vue
+++ view/src/components/SearchBar.vue
@@ -9,18 +9,23 @@
       </b-col>
     </b-row>
     <b-row class="mt-4">
-      <b-col cols="7" md="4" class="mt-2">
-        <p><strong>候補</strong><br><span class="small text-muted">頻度順，前方一致</span></p>
+      <b-col cols="4" xl="2" class="mt-2">
+        <p><strong>使用頻度</strong>&nbsp;<b-spinner variant="secondary" type="grow" small v-if="suggesting" /><br><span class="small text-muted">頻度順，前方一致</span></p>
         <!-- eslint-disable-next-line -->
         <Suggest v-for="suggest in suggests" v-bind:suggest="suggest" v-bind:keyword="debounceKeyword" @use="useName" />
         <p v-if="suggests && suggests.length <= 0">見つかりませんでした．</p>
       </b-col>
-      <b-col cols="5" md="2" class="mt-2">
-        <p><strong>類語</strong></p>
-        <p>未実装</p>
+      <b-col cols="8" xl="6" class="mt-2">
+        <p><strong>各単語ごとの類語</strong>&nbsp;<b-spinner variant="secondary" type="grow" small v-if="synonyming" /><br><span class="small text-muted">日本語WordNetによる</span></p>
+        <b-container fluid>
+          <b-row>
+            <!-- eslint-disable-next-line -->
+            <SynonymKey v-for="(synonym, word) in synonyms" v-bind:synonym="synonym" v-bind:keyword="word" />
+          </b-row>
+        </b-container>
       </b-col>
-      <b-col cols="12" md="6" class="mt-2">
-        <p><strong>使用箇所 <span v-if="results && results.length > 0 && results.length > 100">(100〜 件)</span><span v-if="results && results.length > 0 && results.length <= 100">({{results.length}} 件)</span></strong><br><span class="small text-muted">辞書順，前方一致，大文字小文字を区別</span></p>
+      <b-col cols="12" xl="4" class="mt-2">
+        <p><strong>使用箇所 <span v-if="results && results.length > 0 && results.length > 100">(100〜 件)</span><span v-if="results && results.length > 0 && results.length <= 100">({{results.length}} 件)</span></strong>&nbsp;<b-spinner variant="secondary" type="grow" small v-if="resulting" /><br><span class="small text-muted">辞書順，前各単語ごとの方一致，大文字小文字を区別</span></p>
         <!-- eslint-disable-next-line -->
         <Result v-for="result in results" v-bind:result="result" v-bind:keyword="debounceKeyword" />
         <p v-if="results && results.length <= 0">{{debounceKeyword}} にマッチする結果が見つかりませんでした．</p>
@@ -34,18 +39,24 @@ import Axios from 'axios'
 import _ from 'lodash'
 import Result from './Result.vue'
 import Suggest from './Suggest.vue'
+import SynonymKey from './SynonymKey.vue'
+import lemmatizer from 'wink-lemmatizer'
 
 export default {
   name: 'SearchBar',
   data: function () {
-    return { keyword: '', debounceKeyword: '' }
+    return { keyword: '', debounceKeyword: '', suggesting: false, synonyming: false, resulting: false }
   },
   components: {
     Result: Result,
-    Suggest: Suggest
+    Suggest: Suggest,
+    SynonymKey: SynonymKey
   },
   watch: {
     keyword: function (val) {
+      this.suggesting = true
+      this.synonyming = true
+      this.resulting = true
       this.debouncer()
     }
   },
@@ -62,12 +73,51 @@ export default {
     async results () {
       const keyword = this.debounceKeyword
       const res = await Axios.get('/v1/search?q=' + keyword)
+      this.resulting = false
       return res.data[keyword] || []
     },
     async suggests () {
       const keyword = this.debounceKeyword
       const res = await Axios.get('/v1/suggest?q=' + keyword)
+      this.suggesting = false
       return res.data || []
+    },
+    async synonyms () {
+      var results = {}
+
+      if (this.debounceKeyword === '') {
+        this.synonyming = false
+        return results
+      }
+
+      const keywords = this.debounceKeyword.toLowerCase().split('_')
+
+      for (let keyword of keywords) {
+        const adjectiveLemma = lemmatizer.adjective(keyword)
+        const nounLemma = lemmatizer.noun(keyword)
+        const verbLemma = lemmatizer.verb(keyword)
+
+        var lemmas = []
+        if (adjectiveLemma !== keyword) { lemmas.push(adjectiveLemma) }
+        if (nounLemma !== keyword) { lemmas.push(nounLemma) }
+        if (verbLemma !== keyword) { lemmas.push(verbLemma) }
+        if (lemmas.length === 0) { lemmas.push(keyword) }
+
+        var result = {}
+        for (let lemma of lemmas) {
+          const res = await Axios.get('/v1/synonym?q=' + lemma)
+          if (res.data == null) { continue }
+
+          for (let v of res.data) {
+            if (!(v.Synset in result)) { result[v.Synset] = {} }
+            if (!(v.Lang in result[v.Synset])) { result[v.Synset][v.Lang] = [] }
+            result[v.Synset][v.Lang].push(v.Name)
+          }
+        }
+        results[keyword] = result
+      }
+      this.synonyming = false
+      return results
     }
   }
 }
