11b414346896869a22c4ebefecf1878cb70007b2
typewriter <takuyaxp@gmail.com>
2019-02-28 23:44:51 +0900

    [BETA] Add favicon

--- view/src/components/Title.vue
+++ view/src/components/Title.vue
@@ -3,10 +3,10 @@
     <b-row class="bg-light">
       <b-col cols="12">
         <h1>
-          Zenrei <small>alpha (Ruby only)</small>
+          Zenrei <span class="small text-muted">beta</span>
         </h1>
         <!-- <small>(from Japanese: 前例, "precedent")</small> -->
-        <p>GitHubのスター数上位リポジトリで，その変数名はどのくらい使われているか？</p>
+        <p class="small"><b-badge>Ruby only</b-badge> クラス名，メソッド名，変数名の使用実績をGitHubスター数上位リポジトリから検索</p>
       </b-col>
     </b-row>
   </b-container>
