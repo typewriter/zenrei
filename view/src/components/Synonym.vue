7b51fc845f12a35c7f2666ce785ac1509ae554c2
typewriter <takuyaxp@gmail.com>
2019-02-28 23:45:14 +0900

    Oops! Add forgotten files

---
+++ view/src/components/Synonym.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <b-card class="result-card small">
+      <b-card-text>
+        {{synonym["eng"] && synonym["eng"].join(', ')}}<br />
+        <span class="text-muted">{{synonym["jpn"] && synonym["jpn"].join('，')}}</span>
+      </b-card-text>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Synonym',
+  props: ['synonym']
+}
+</script>
+
+<style scoped>
+.card-body {
+  padding: 0.25rem;
+}
+.result-card {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+</style>
---
+++ view/src/components/SynonymKey.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-col cols="12" md="6">
+    <b-card class="result-card">
+      <b-card-text>
+        <strong>{{keyword}}</strong>
+        <span v-if="synonym.length === 0">見つかりませんでした．</span>
+      </b-card-text>
+      <Synonym v-for="synonymItem in synonym" v-bind:synonym="synonymItem" />
+    </b-card>
+  </b-col>
+</template>
+
+<script>
+import Synonym from './Synonym.vue'
+
+export default {
+  name: 'SynonymKey',
+  props: ['synonym', 'keyword'],
+  components: {
+    Synonym: Synonym
+  }
+}
+</script>
+
+<style scoped>
+.card-body {
+  padding: 0.5rem;
+}
+</style>
