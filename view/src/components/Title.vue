8301c75e895a505d93cd4a3b25d919cc7969c1ed
typewriter <takuyaxp@gmail.com>
2019-03-02 19:43:20 +0900

    Update description

--- view/src/components/Title.vue
+++ view/src/components/Title.vue
@@ -6,7 +6,7 @@
           Zenrei <span class="small text-muted">beta</span>
         </h1>
         <!-- <small>(from Japanese: 前例, "precedent")</small> -->
-        <p class="small"><b-badge>Ruby only</b-badge> クラス名，メソッド名，変数名の使用実績をGitHubスター数上位リポジトリから検索</p>
+        <p class="small"><b-badge>Ruby only</b-badge> クラス名，メソッド名，変数名の使用実績をGitHubスター数上位リポジトリから検索 (<a href="https://github.com/typewriter/zenrei#api">API版</a>)</p>
       </b-col>
     </b-row>
   </b-container>
